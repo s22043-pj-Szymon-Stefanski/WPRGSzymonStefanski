403bd6a5514e2742e898de7943ef8aa1954e9e87
s22043-pj-Szymon-Stefanski <73427627+s22043-pj-Szymon-Stefanski@users.noreply.github.com>
2022-04-08 18:22:50 +0200

    Add files via upload

---
+++ z2/index.html/z2.1.php
@@ -0,0 +1,40 @@
+
+<FORM method="POST" action="">
+        <INPUT TYPE="NUMBER" name="liczbaP" size="10">
+        <INPUT TYPE="NUMBER" name="liczbaD" size="10">
+        <br>
+        <select name="dzialania">
+            <option value="dodawanie">dodawanie</option>
+            <option value="odejmowanie">odejmowanie</option>
+            <option value="dzielenie">dzielenie</option>
+            <option value="mnozenie">mnozenie</option>
+        </select>
+        <br>
+        <INPUT TYPE=SUBMIT>
+        <OUTPUT TYPE=NUMBER>
+</FORM>
+
+<?php
+$lP = $_POST['liczbaP'];
+$lD = $_POST['liczbaD'];
+$dzialania = $_POST['dzialania'];
+$w = "";
+
+if ($dzialania == "dodawanie") {
+    $w = $lP + $lD;
+}
+
+if ($dzialania == "odejmowanie") {
+    $w = $lP - $lD;
+}
+
+if ($dzialania == "dzielenie") {
+    $w = $lP / $lD;
+}
+
+if ($dzialania == "mnozenie") {
+    $w = $lP * $lD;
+}
+echo ("<br>");
+echo $w;
+?>
